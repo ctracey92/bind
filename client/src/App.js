95d5a9bf8a9eb13000441517d7121fdf80d54e36
Cody <cody.tracy92@gmail.com>
2019-06-17 21:50:24 -0500

    reformatted my router/routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,27 @@ import {
     BrowserRouter as Router,
     Route,
 } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Login from "./pages/Login";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 
 class App extends Component {
-    state = {
-        loggedIn: false,
-
-    }
-
     render(){
         return(
-            <div>
-                <Navbar />
-                <Landing />                
-            </div>
-        ) 
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                </div>                
+            </Router>
+        ); 
     }
-
 }
 
 export default App;
