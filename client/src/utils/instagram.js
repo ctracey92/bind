cb0e8dd8cd1f3f544535e74e25569c0391663bfd
Cody <cody.tracy92@gmail.com>
2019-07-25 13:18:13 -0500

    fixed a hardcoded path

--- client/src/utils/instagram.js
+++ client/src/utils/instagram.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     authorize: () => {
-        return axios.get("http://127.0.0.1:3001/api/connect/instagram" )
+        return axios.get("/api/connect/instagram" )
     },
     getHashtags: () => {
         return axios.get("/api/connect/scrape/instagram")
